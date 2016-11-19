a4248fb3d3d5750d3ca8a438203e8b8343986425
iannicktl <iannick@hotmail.ca>
2016-11-19 11:30:20 -0500

    add unit  test project

---
+++ MonsterInc/MonsterInc/MonsterInc/Shared/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace MonsterInc.Shared
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objectToSerialize"></param>
+        /// <param name="nameOfFile"></param>
+        /// <param name="defaultPath">System.Reflection.Assembly.GetExecutingAssembly().Location</param>
+        /// <returns></returns>
+        public static string XmlSerialize<T>(this T objectToSerialize, string nameOfFile, bool defaultPath )
+        {
+            if (!nameOfFile.ToLower().Contains(".xml"))
+            {
+                nameOfFile = nameOfFile + ".xml";
+            }
+            string directory ="";
+            if (defaultPath)
+            {
+                directory = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            }
+            string fullPath = $@"{directory}\{nameOfFile}";
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            StringWriter stringWriter = new StringWriter();
+            XmlTextWriter xmlWriter = new XmlTextWriter(stringWriter);
+
+            xmlWriter.Formatting = Formatting.Indented;
+            xmlSerializer.Serialize(xmlWriter, objectToSerialize);
+
+            return stringWriter.ToString();
+        }
+    }
+
+
+}
+
---
+++ MonsterInc/MonsterInc/UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MonsterInc;
+using MonsterInc.Shared;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<MonsterInc.Item>  a = MonsterInc.ItemData.Items ;
+            a.XmlSerialize<List<MonsterInc.Item>>("", true);
+            
+        }
+    }
+}
